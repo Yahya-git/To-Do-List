6ec7d5dc313f73969fce9111e5d78ddf7330610b
Yahya <yahya.19of99@gmail.com>
2023-03-17 15:03:43 +0500

    dev-004: added mail settings to config.py

--- app/config.py
+++ app/config.py
@@ -2,11 +2,20 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
+    url: str
     db_username: str
     db_password: str
     db_hostname: str
     db_port: str
     db_name: str
+    mail_username: str
+    mail_password: str
+    mail_from: str
+    mail_port: str
+    mail_server: str
+    mail_tls: bool
+    mail_ssl: bool
+    use_credentials: bool
 
     class Config:
         env_file = ".env"
