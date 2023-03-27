b6769e5b9c4b4c7db53d618470bf8fcaa5229b5a
Yahya <yahya.19of99@gmail.com>
2023-03-27 14:46:56 +0500

    dev-006: merged oauth2.py into utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,10 +1,69 @@
-from typing import List
+from datetime import datetime, timedelta
 
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from fastapi_mail import ConnectionConfig, FastMail, MessageSchema
+from fastapi_sso.sso.google import GoogleSSO
+from jose import JWTError, jwt
 from passlib.context import CryptContext
-from pydantic import BaseModel, EmailStr
+from sqlalchemy.orm import Session
+
+from app.database import database, models
 
 from .config import settings
+from .schemas import schemas_misc
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_TIME = settings.access_token_expire_time
+
+google_sso = GoogleSSO(
+    client_id=settings.google_client_id,
+    client_secret=settings.google_client_secret,
+    redirect_uri=settings.redirect_url,
+    allow_insecure_http=True,
+    scope=["openid", "email", "profile"],
+)
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_TIME)
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def verify_access_token(token: str, credentials_exception):
+    try:
+        decoded_jwt = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id: str = decoded_jwt.get("user_id")
+        if id is None:
+            raise credentials_exception
+        token_data = schemas_misc.TokenData(id=id)
+    except JWTError:
+        raise credentials_exception
+    return token_data
+
+
+def get_current_user(
+    # trunk-ignore(ruff/B008)
+    token: str = Depends(oauth2_scheme),
+    # trunk-ignore(ruff/B008)
+    db: Session = Depends(database.get_db),
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail=f'{"could not validate credentials"}',
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    token = verify_access_token(token, credentials_exception)
+    user = db.query(models.User).filter(models.User.id == token.id).first()
+    return user
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -15,10 +74,6 @@ def hash(password: str):
 
 def verify(attempted_password, hashed_password):
     return pwd_context.verify(attempted_password, hashed_password)
-
-
-class Email(BaseModel):
-    email: List[EmailStr]
 
 
 conf = ConnectionConfig(
@@ -33,7 +88,9 @@ conf = ConnectionConfig(
 )
 
 
-async def send_mail(email: Email, link: str, subject_template: str, template: str):
+async def send_mail(
+    email: schemas_misc.Email, link: str, subject_template: str, template: str
+):
     message = MessageSchema(
         subject=subject_template, recipients=[email], body=template, subtype="html"
     )
