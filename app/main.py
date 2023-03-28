86a96d5d4c1782502086ac6078f46724ef5da6ad
Yahya <yahya.19of99@gmail.com>
2023-03-28 11:14:55 +0500

    dev-006: updated main.py to include router for auth.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
+import os
+
 from fastapi import FastAPI
 
-from .routers import users
+from .routers import auth, users
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
 app = FastAPI()
 
 app.include_router(users.router)
+app.include_router(auth.router)
 
 
 @app.get("/")
