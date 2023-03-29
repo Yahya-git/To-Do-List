7625a29555e7828a3164ee2a14919ca904b074a2
Yahya <yahya.19of99@gmail.com>
2023-03-29 10:53:55 +0500

    dev-007: created pytest fixtures for different use cases and set up a test database

---
+++ tests/conftest.py
@@ -0,0 +1,74 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.config import settings
+from app.database import models
+from app.database.database import Base, get_db
+from app.main import app
+from app.schemas import schemas_users
+
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{settings.db_username}:{settings.db_password}@{settings.db_hostname}:{settings.db_port}/{settings.db_name}_test"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+TestSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture
+def session():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    db = TestSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture
+def client(session):
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
+
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
+
+
+@pytest.fixture
+def test_user(client):
+    user_data = {"email": "yahya.19of99@gmail.com", "password": "hello"}
+    response = client.post("/users/", json=user_data)
+    assert response.status_code == 201
+    user = response.json()
+    user["password"] = user_data["password"]
+    return schemas_users.User(**user)
+
+
+@pytest.fixture
+def test_user_update(client):
+    user_data = {"email": "yahya.19of99@gmail.com", "password": "hello"}
+    response = client.post("/users/", json=user_data)
+    models.Verification.__table__.drop(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    assert response.status_code == 201
+    user = response.json()
+    user["password"] = user_data["password"]
+    return schemas_users.User(**user)
+
+
+@pytest.fixture
+def test_user_login(client):
+    user_data = {"email": "yahya.19of99@gmail.com", "password": "hello"}
+    response = client.post("/users/", json=user_data)
+    models.Verification.__table__.drop(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    models.User.is_verified = True
+    assert response.status_code == 201
+    user = response.json()
+    user["password"] = user_data["password"]
+    return schemas_users.UserCreate(**user)
