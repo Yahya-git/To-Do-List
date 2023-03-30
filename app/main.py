2d67a7af9da3852dda24c1f8310ef2b5f227803e
Yahya <yahya.19of99@gmail.com>
2023-03-30 13:32:24 +0500

    dev-009: updated main.py to include router for tasks.py and added middleware

--- app/main.py
+++ app/main.py
@@ -1,14 +1,26 @@
 import os
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from .routers import auth, users
+from .routers import auth, tasks, users
 
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(users.router)
+app.include_router(tasks.router)
 app.include_router(auth.router)
 
 
