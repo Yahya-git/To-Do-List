ac84e0f549a3cb13a5ea4d4f6ee8f16f172c601c
Yahya <yahya.19of99@gmail.com>
2023-03-31 15:32:23 +0500

    dev-010: added logging middleware to main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
+import logging
 import os
+from logging.handlers import RotatingFileHandler
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import auth, tasks, users
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+file_handler = RotatingFileHandler("app.log", maxBytes=10000000, backupCount=5)
+file_handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
@@ -18,6 +28,15 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    logger.info(f"Incoming Request: {request.method} {request.url}")
+    response = await call_next(request)
+    logger.info(f"Outgoing Response: {response.status_code}")
+    return response
+
 
 app.include_router(users.router)
 app.include_router(tasks.router)
