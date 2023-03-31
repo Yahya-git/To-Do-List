92a1824f8b323cc969cb4684cfec6572c6146034
Yahya <yahya.19of99@gmail.com>
2023-03-31 11:41:34 +0500

    dev-010: added basic tests for tasks like creating, updating and deleting

---
+++ tests/test_tasks.py
@@ -0,0 +1,43 @@
+import pytest
+
+from app.schemas import schemas_tasks
+
+
+@pytest.mark.parametrize(
+    "title, description",
+    [
+        ("new title 1", "new description 1"),
+        ("new title 2", "new description 2"),
+        ("new title 3", "new description 3"),
+    ],
+)
+def test_create_tasks(
+    authorized_client,
+    test_user,
+    title,
+    description,
+):
+    res = authorized_client.post(
+        f'{"/tasks/"}', json={"title": title, "description": description}
+    )
+    print(res.json())
+    created_post = schemas_tasks.Task(**res.json())
+    assert res.status_code == 201
+    assert created_post.title == title
+    assert created_post.description == description
+    assert created_post.user_id == test_user.id
+
+
+def test_update_task(authorized_client, test_user, test_task):
+    res = authorized_client.patch(
+        f"/tasks/{test_task[0].id}", json={"title": "new title"}
+    )
+    assert res.status_code == 202
+    updated_task = schemas_tasks.Task(**res.json())
+    assert updated_task.title == "new title"
+    assert updated_task.user_id == test_user.id
+
+
+def test_delete_task(authorized_client, test_task):
+    res = authorized_client.delete(f"/tasks/{test_task[0].id}")
+    assert res.status_code == 204
